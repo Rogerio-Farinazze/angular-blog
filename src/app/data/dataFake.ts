6e27cf0da430a4f587f7549cdd51aa71b5f88e0d
rafarinazze <rafarinazze@gmail.com>
2023-05-26 16:32:19 -0300

    Created dinamic page content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Titulo 1 ",
+    "description": "Texto 1",
+    "photoCover": "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+  },
+  {
+    "id":"2",
+    "title": "Titulo 2",
+    "description": "Texto 2",
+    "photoCover": "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+  },
+  {
+    "id":"3",
+    "title": "Titulo 3",
+    "description": "Texto 3",
+    "photoCover": "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+  },
+  {
+    "id":"4",
+    "title": "Titulo 4",
+    "description": "Texto 4",
+    "photoCover": "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+  },
+]
